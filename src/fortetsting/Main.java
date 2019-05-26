25d6ce5340aaa8758719d49414f4c0f748552461
user <user@shytan-mashina>
2019-05-26 23:00:01 +0400

    unity

--- src/fortetsting/Main.java
+++ src/fortetsting/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, slavane!!!");
         System.out.println("It`s model breanch");
+        System.out.println("It`s unity breanchs");
     }
 }
